99725a1e9d75384077112e15f8fe1f3ae55e3d8d
Ricky Mach <machricky2002@gmail.com>
2024-12-12 16:34:07 -0500

    added ggst info

--- vite/src/gameComponents/GGSTContent.tsx
+++ vite/src/gameComponents/GGSTContent.tsx
@@ -3,9 +3,35 @@ import { Characters } from "../components/Characters";
 import { RightContent } from "../components/RightContent";
 import { useState } from "react";
 // character images
-import solImage from "../assets/GGST/SolBadguy.png";
-import kyImage from "../assets/GGST/KyKiske.png";
-import mayImage from "../assets/GGST/May.png";
+import solImage from "../assets/GGST/sol.png";
+import kyImage from "../assets/GGST/ky.png";
+import mayImage from "../assets/GGST/may.png";
+import axlImage from "../assets/GGST/axl.png";
+import chippImage from "../assets/GGST/chipp.png";
+import potImage from "../assets/GGST/pot.png";
+import faustImage from "../assets/GGST/faust.png";
+import milImage from "../assets/GGST/millia.png";
+import zatoImage from "../assets/GGST/zato.png";
+import ramImage from "../assets/GGST/ram.png";
+import leoImage from "../assets/GGST/leo.png";
+import nagoImage from "../assets/GGST/nago.png";
+import gioImage from "../assets/GGST/gio.png";
+import anjiImage from "../assets/GGST/anji.png";
+import inoImage from "../assets/GGST/ino.png";
+import goldImage from "../assets/GGST/gold.png";
+import jackoImage from "../assets/GGST/jacko.png";
+import hcImage from "../assets/GGST/hc.png";
+import baikenImage from "../assets/GGST/baiken.png";
+import testImage from "../assets/GGST/test.png";
+import brisketImage from "../assets/GGST/brisket.png";
+import sinImage from "../assets/GGST/sin.png";
+import bedImage from "../assets/GGST/bed.png";
+import asukaImage from "../assets/GGST/asuka.png";
+import johnImage from "../assets/GGST/johnny.png";
+import elpheltImage from "../assets/GGST/elphelt.png";
+import abaImage from "../assets/GGST/aba.png";
+import slayerImage from "../assets/GGST/slayer.png";
+import dizzyImage from "../assets/GGST/dizzy.png";
 import gameLogo from "../assets/GGST/ggstlogo.png";
 import gameInfo from "../data/ggstGameinfo.json";
 import CharacterInfos from "../data/ggstCharactersInfo.json";
@@ -27,19 +53,120 @@ export const GGSTContent = () => {
   // Characters in the game and images
   let characters = [
     {
-      name: "Sol Badguy",
+      name: "Sol",
       image: solImage,
-      color: "#8b0000",
     },
     {
-      name: "Ky Kiske",
+      name: "Ky",
       image: kyImage,
-      color: "#318CE7",
     },
     {
       name: "May",
       image: mayImage,
-      color: "#FF8C00",
+    },
+    {
+      name: "Axl",
+      image: axlImage,
+    },
+    {
+      name: "Chipp",
+      image: chippImage,
+    },
+    {
+      name: "Potemkin",
+      image: potImage,
+    },
+    {
+      name: "Faust",
+      image: faustImage,
+    },
+    {
+      name: "Millia",
+      image: milImage,
+    },
+    {
+      name: "Zato-1",
+      image: zatoImage,
+    },
+    {
+      name: "Ramlethal",
+      image: ramImage,
+    },
+    {
+      name: "Leo",
+      image: leoImage,
+    },
+    {
+      name: "Nagoriyuki",
+      image: nagoImage,
+    },
+    {
+      name: "Giovanna",
+      image: gioImage,
+    },
+    {
+      name: "Anji",
+      image: anjiImage,
+    },
+    {
+      name: "I-No",
+      image: inoImage,
+    },
+    {
+      name: "Goldlewis",
+      image: goldImage,
+    },
+    {
+      name: "Jack-O'",
+      image: jackoImage,
+    },
+    {
+      name: "Happy Chaos",
+      image: hcImage,
+    },
+    {
+      name: "Baiken",
+      image: baikenImage,
+    },
+    {
+      name: "Testament",
+      image: testImage,
+    },
+    {
+      name: "Bridget",
+      image: brisketImage,
+    },
+    {
+      name: "Sin",
+      image: sinImage,
+    },
+    {
+      name: "Bedman?",
+      image: bedImage,
+    },
+    {
+      name: "Asuka R#",
+      image: asukaImage,
+    },
+    {
+      name: "Johnny",
+      image: johnImage,
+    },
+    {
+      name: "Elphelt",
+      image: elpheltImage,
+    },
+    {
+      name: "A.B.A",
+      image: abaImage,
+    },
+    {
+      name: "Slayer",
+      image: slayerImage,
+    },
+    {
+      name: "Dizzy",
+      image: dizzyImage,
     },
   ];
 
@@ -72,7 +199,7 @@ export const GGSTContent = () => {
           style={
             {
               borderColor: selectedCharInfo?.color || "#555",
-              fontSize: "3.5vh", // adjust values based on font
+              fontSize: "3vh", // adjust values based on font
               letterSpacing: "0vw",
               "--active-color": selectedCharInfo?.color || "rgb(121, 238, 121)",
             } as React.CSSProperties
